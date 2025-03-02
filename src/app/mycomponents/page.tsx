014aec140ea4dc82c60e4b5439ea95f00d7866a0
benzinamohamed <am.benzina@ensta.edu.dz>
2025-03-02 14:48:11 +0100

    adding the components page

---
+++ src/app/mycomponents/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+
+import React from 'react'
+
+
+function MyComponents() {
+  const components = [
+    {
+      name: "Button",
+      description: "A clickable UI element used to trigger actions or submit forms"
+    },
+    {
+      name: "Input Field",
+      description: "Text input component for collecting user data and free-form responses"
+    },
+    {
+      name: "Navbar",
+      description: "Navigation bar component containing links to main sections of a website"
+    },
+    {
+      name: "Card",
+      description: "Container component displaying content and actions about a single subject"
+    },
+    {
+      name: "Modal",
+      description: "Dialog box/popup window displayed on top of the main content"
+    },
+    {
+      name: "Dropdown Menu",
+      description: "Interactive component that reveals a list of options when activated"
+    },
+    {
+      name: "Progress Bar",
+      description: "Visual indicator showing completion status of a task or process"
+    }
+  ];
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyComponents
